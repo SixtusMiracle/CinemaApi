80489a5329a5f1c5713247032b04d28d55fd9512
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-01-30 00:26:54 +0100

    updating movies feature

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -37,5 +37,41 @@ namespace CinemaApi.Controllers
       return StatusCode(StatusCodes.Status201Created);
     }
 
+    // PUT: api/<MoviesController>/5
+    [Authorize(Roles = "Admin")]
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromForm] Movie movieObj)
+    {
+      var movie = _dbContext.Movies.Find(id);
+      if (movie == null)
+      {
+        return NotFound("No record found against this id");
+      }
+      else
+      {
+        var guid = Guid.NewGuid();
+        var filePath = Path.Combine("wwwroot", guid + ".jpg");
+        if (movieObj.Image != null)
+        {
+          var fileStream = new FileStream(filePath, FileMode.Create);
+          movieObj.Image.CopyTo(fileStream);
+          movieObj.ImageUrl = filePath.Remove(0, 7);
+        }
+
+        movie.Name = movieObj.Name;
+        movie.Description = movieObj.Description;
+        movie.Language = movieObj.Language;
+        movie.Duration = movieObj.Duration;
+        movie.PlayingDate = movieObj.PlayingDate;
+        movie.PlayingTime = movieObj.PlayingTime;
+        movie.Rating = movieObj.Rating;
+        movie.Genre = movieObj.Genre;
+        movie.TrailorUrl = movieObj.TrailorUrl;
+        movie.TicketPrice = movieObj.TicketPrice;
+        _dbContext.SaveChanges();
+        return Ok("Record updated successfuly");
+      }
+    }
+
   }
 }
